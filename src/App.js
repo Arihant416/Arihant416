0645377b5781a3513f6aa860556952ca721f4f36
Arihant416 <arihantjain416@gmail.com>
2025-02-01 23:31:18 +0530

    Re-route wrong endpoint to home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom'; // No need for BrowserRouter import here
+import { Route, Routes , Navigate} from 'react-router-dom'; // No need for BrowserRouter import here
 import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './components/About';
@@ -19,6 +19,7 @@ function App() {
 				<Route path='/projects' element={<Projects />} />
 				<Route path='/skills' element={<Skills />} />
 				<Route path='/contact' element={<Contact />} />
+				<Route path='*' element={<Navigate to='/' replace />} />
 			</Routes>
 			<Footer />
 		</div>
